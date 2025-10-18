e836facb969150fffe8ec192e8bced488c08b9c3
vikried <vikried@gmx.de>
2025-10-18 20:53:24 +0200

    Style Tailwind

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -4,6 +4,7 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
 import { createPinia } from 'pinia'
+import './style.css'
 
 createApp(App)
   .use(router)
--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -1,1 +1,11 @@
-content: ["./index.html", "./src/**/*.{vue,js,ts,jsx,tsx}"]
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./index.html",
+    "./src/**/*.{vue,js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
